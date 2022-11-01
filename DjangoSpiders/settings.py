9e98456abf95f0a379f5655bdb03c7b48373d241
dominoar <duominuoaier@gmail.com>
2022-11-01 19:11:22 +0800

    更新爬虫添加到主内容中

--- DjangoSpiders/settings.py
+++ DjangoSpiders/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-sqhworjcw0vkeib=$z1!1uc07%@28vbq+yhof#p)(#zo3xd-p^"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost", "ycyspace.cn", "ycychan.com"]
 
