d8e9aa9d02ddaba4392adb58e0aa917691624e58
多米诺艾尔 <duominuoaier@gamil.com>
2022-11-15 21:16:46 +0800

    添加

--- DjangoSpiders/settings.py
+++ DjangoSpiders/settings.py
@@ -111,10 +111,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'app01/static/'),
+    os.path.join(BASE_DIR, 'static'),
+    'app01/static'
 )
 
 # Default primary key field type
